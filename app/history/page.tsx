f7f78fbc358afd04e6e0ee56737a3d9f416ce413
Mostafa-Nemati <mostafanemati1998@gmail.com>
2025-11-24 16:40:03 +0330

    add dashboard

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -8,8 +8,8 @@ export default function Page() {
         <>
             <div className="flex flex-col">
                 <Link href={'/link'} className="flex items-center gap-x-1 mb-4">
-                    <BiChevronLeft className="text-2xl" />
-                    <h3>HOME</h3>
+                    {/*<BiChevronLeft className="text-2xl" />*/}
+                    <h3>HISTORY</h3>
                 </Link>
                 <div className="bg-card p-3 rounded-lg">
                     <Calender />
